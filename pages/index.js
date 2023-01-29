bfbf4dc960a8fc7af72abfc0bd8dce441cd1f454
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-29 10:03:47 +0530

    Add navbar, blogs skeleton and styles

--- pages/index.js
+++ pages/index.js
@@ -12,75 +12,36 @@ export default function Home() {
         <title>Scribblespot</title>
         <meta
           name='description'
-          content="A community for developers to share, learn and grow together."
+          content='A community for developers to share, learn and grow together.'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <nav class={styles.mainNav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <main className={styles.main}>
         <h1>Scribblespot</h1>
         <div className={styles.description}>
           <p>Where developers come to stay ahead of the curve.</p>
         </div>
-
-
-        <div className={styles.grid}>
-          <a
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.blogs}>
+          <div className={styles.blogItem}>
+            <h3>Blog 1</h3>
+            <p>Description..</p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3>Blog 1</h3>
+            <p>Description..</p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3>Blog 1</h3>
+            <p>Description..</p>
+          </div>
         </div>
       </main>
     </>
