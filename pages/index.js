ffc117b5f40e252fca3b38a8b7e45be5b2a8b449
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-29 10:16:52 +0530

    Add multiple pages routes

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <nav class={styles.mainNav}>
+      <nav className={styles.mainNav}>
         <ul>
           <li>Home</li>
           <li>About</li>
