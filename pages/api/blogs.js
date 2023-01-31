9500484b0fe8ef5adddd5162bab70a7038f5f70a
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-31 11:20:42 +0530

    Use api endpoints to fetch blogs

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,7 +1,13 @@
 import * as fs from 'fs'
 
-export default function handler(req, res) {
-  fs.readdir('blogdata', (err, data) => {
-    res.status(200).json(data)
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir('blogdata')
+  let blogs = []
+  let blog
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i]
+    blog = await fs.promises.readFile(`blogdata/${item}`, 'utf-8')
+    blogs.push(JSON.parse(blog))
+  }
+  res.status(200).json(blogs)
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,31 +1,36 @@
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
+
 import styles from '../styles/Blog.module.css'
 
 // TODO: Collect all the files from blogdata directory
 // TODO: Iterate through them and display them
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:3000/api/blogs')
+      const data = await res.json()
+      setBlogs(data)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className='main'>
       <div className={styles.blogs}>
-        <Link href='/blogpost/blog1'>
-          <div className={styles.card}>
-            <h2>Blog 1</h2>
-            <p>Description..</p>
-          </div>
-        </Link>
-        <Link href='/blogpost/blog2'>
-          <div className={styles.card}>
-            <h2>Blog 2</h2>
-            <p>Description..</p>
-          </div>
-        </Link>
-        <Link href='/blogpost/blog3'>
-          <div className={styles.card}>
-            <h2>Blog 3</h2>
-            <p>Description..</p>
-          </div>
-        </Link>
+        {blogs.map((blog) => {
+          return (
+            <Link href={`/blogpost/${blog.slug}`} key={blog.slug}>
+              <div className={styles.card}>
+                <h2>{blog.title}</h2>
+                <p>{blog.content.substring(0, 150)}...</p>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
