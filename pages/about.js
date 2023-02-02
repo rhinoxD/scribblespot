b09943812a72176017786810feeafa72bfb85d4d
Shivam Kumar <coolshiv0721@gmail.com>
2023-02-02 19:43:42 +0530

    Designing the about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,45 @@
+import styles from '@/styles/About.module.css'
+
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className='main'>
+      <h1>About Scribblespot</h1>
+      <h2 className={styles.h2}>Introduction</h2>
+      <p className={styles.p}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ab
+        molestias optio itaque. Nobis est vero amet ipsa tempore, aut natus,
+        voluptatibus possimus laudantium ab error facilis iusto eveniet ducimus,
+        aliquid nesciunt repudiandae modi! Perspiciatis sapiente qui harum ad
+        obcaecati! Ullam aut libero explicabo debitis suscipit amet asperiores
+        aliquid ab, fugiat enim atque veniam obcaecati tempora laudantium
+        similique expedita soluta ipsa minus nihil rem. Magni ipsum dignissimos
+        labore, ipsa quo reprehenderit voluptates veritatis alias, velit fuga
+        consequatur aliquam modi adipisci nostrum eos praesentium beatae impedit
+        nulla excepturi. Officia ipsa accusamus facilis unde odit corporis.
+        Fugit nisi deleniti voluptatem ratione cupiditate?
+      </p>
+
+      <h2 className={styles.h2}>Services Offered</h2>
+      <p className={styles.p}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+        accusantium pariatur! Quidem, nemo. A totam, molestias quas autem
+        beatae, expedita deleniti quidem doloribus vero perspiciatis excepturi
+        accusamus voluptas itaque laboriosam repellendus tempore. Hic pariatur,
+        quaerat quae ducimus cum officiis cumque maiores ad facilis eligendi,
+        minus ratione non, odit neque! Atque.
+      </p>
+
+      <h2 className={styles.h2}>Contact Us</h2>
+      <p className={styles.p}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+        accusantium pariatur! Quidem, nemo. A totam, molestias quas autem
+        beatae, expedita deleniti quidem doloribus vero perspiciatis excepturi
+        accusamus voluptas itaque laboriosam repellendus tempore. Hic pariatur,
+        quaerat quae ducimus cum officiis cumque maiores ad facilis eligendi,
+        minus ratione non, odit neque! Atque.
+      </p>
+    </div>
+  )
 }
 
 export default About
